6aeb2c4e26a449130996f6a3962578e1399dadf6
come-collab <cometrisse640@gmail.com>
2024-09-03 14:02:03 +0200

    well we have a start

--- Manipulating Excels with Code/Front.py
+++ Manipulating Excels with Code/Front.py
@@ -1,2 +1,11 @@
 import streamlit as st
 st.title("Manipulating excels with code")
+col1, col2, col3 = st.columns([1,1,1])
+with col1:
+    st.text_input(label="Nom")
+with col2:
+    st.text_input(label = "Kill")
+with col3:
+    st.text_input(label= "Points")
+
+
