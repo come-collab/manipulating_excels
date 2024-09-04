8c81686ac5a4c7c141d9b04e5de63b75a1498d8e
come-collab <cometrisse640@gmail.com>
2024-09-04 09:04:50 +0200

    init fronted

--- Manipulating Excels with Code/Front.py
+++ Manipulating Excels with Code/Front.py
@@ -1,11 +1,42 @@
 import streamlit as st
-st.title("Manipulating excels with code")
-col1, col2, col3 = st.columns([1,1,1])
-with col1:
-    st.text_input(label="Nom")
-with col2:
-    st.text_input(label = "Kill")
-with col3:
-    st.text_input(label= "Points")
 
 
+#Adding the credential for every member of the BDF
+
+USER_CREDENTIAL = {
+    "Didier" : "BDF123",
+    "Côme" : "",
+    "Baptiste" : "",
+}
+
+def login(): 
+    st.title("Page de Login")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    login_button = st.button("Login")
+
+    #Check the login credentials
+    if login_button:
+        if username in USER_CREDENTIAL and USER_CREDENTIAL[username] == password:
+            st.session_state['logged_in'] = True
+            st.succces("Vous etes connecté")
+        else: 
+            st.error("Identifiant incorrect")
+
+def autheticated_page():
+    st.title("Welcome to the Dashboard")
+    st.write("You are logged in ")
+
+
+def logout():
+    st.session_state['logged_in'] = False
+
+#Main app 
+if 'logged_in' not in st.session_state:
+    st.session_state['logged_in'] = False
+
+if st.session_state['logged_in']:
+    autheticated_page()
+    st.button("Logout",on_click=logout)
+else:
+    login()
